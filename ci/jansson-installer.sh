50454219cabd5ae579e25ff8082e44dde146f137
Nikolas Thitu <nikolas@hauz.io>
2017-06-17 12:40:26 -0400

    jansson

---
+++ ci/jansson-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+##
+## grab latest Jansson
+##
+wget http://www.digip.org/jansson/releases/jansson-2.10.tar.gz
+tar xzf jansson-2.10.tar.gz
+cd jansson-2.10 && ./configure && make && make check && make install
+
+
