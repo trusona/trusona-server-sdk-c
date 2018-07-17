ebf879086df669f513ec1b6908a00a1b1d9736e8
Nikolas Thitu <nikolas@hauz.io>
2018-07-17 01:34:13 -0400

    auto formatting PAM

--- src/pam/trusona_pam.c
+++ src/pam/trusona_pam.c
@@ -24,15 +24,17 @@
 
 #include "trusona_pam.h"
 
-const char *pam_module_name = "trusona_pam";
+const char *pam_module_name  = "trusona_pam";
 const char *default_settings = "/usr/local/etc/trusona/settings.json";
 
-int trusona_pam(pam_handle_t *pam, int flags, int argc, const char **argv) {
+int trusona_pam(pam_handle_t *pam, int flags, int argc, const char **argv)
+{
   const char *settings = NULL;
   const char *username;
-  int i;
+  int         i;
 
-  struct key_value {
+  struct key_value
+  {
     char *key;
     char *value;
   };
@@ -43,14 +45,14 @@ int trusona_pam(pam_handle_t *pam, int flags, int argc, const char **argv) {
   for (i = 0; i < argc; i++) {
     char *value = (char *)argv[i];
 
-    if(value != NULL) {
+    if (value != NULL) {
       syslog(LOG_NOTICE, "%s: configured key-value pair: %s", pam_module_name, value);
       struct key_value kv;
 
-      kv.key = strtok(value, "=");
+      kv.key   = strtok(value, "=");
       kv.value = strtok(NULL, "=");
 
-      if(kv.key != NULL && kv.value != NULL) {
+      if (kv.key != NULL && kv.value != NULL) {
         syslog(LOG_NOTICE, "%s: parsed key-value pair: %s:%s", pam_module_name, kv.key, kv.value);
 
         if (!settings && strcmp(kv.key, "settings") == 0) {
@@ -62,45 +64,51 @@ int trusona_pam(pam_handle_t *pam, int flags, int argc, const char **argv) {
 
   settings = settings == NULL ? default_settings : settings;
 
-  enum TRUSONA_SDK_RESULT rc = trusonafy(settings, username);
+  enum TRUSONA_SDK_RESULT rc = trusonafy_v1(settings, username);
 
-  if(rc == TRUSONA_SUCCESS) {
+  if (rc == TRUSONA_SUCCESS) {
     fprintf(stderr, "\nTrusonafication succeeded!\n");
-    return PAM_SUCCESS;
+    return(PAM_SUCCESS);
   }
   else {
     fprintf(stderr, "\nTrusonafication failed!\n");
-    return PAM_AUTH_ERR;
+    return(PAM_AUTH_ERR);
   }
 }
 
 /* PAM entry point for authentication - http://linux.die.net/man/3/pam_authenticate */
-PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return trusona_pam(pamh, flags, argc, argv);
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(trusona_pam(pamh, flags, argc, argv));
 }
 
 /* PAM entry point for session creation - http://linux.die.net/man/3/pam_open_session */
-PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return PAM_IGNORE;
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(PAM_IGNORE);
 }
 
 /* PAM entry point for session cleanup - http://linux.die.net/man/3/pam_close_session */
-PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return PAM_SUCCESS;
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(PAM_SUCCESS);
 }
 
 /* PAM entry point for accounting - http://linux.die.net/man/3/pam_acct_mgmt */
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return PAM_SUCCESS;
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(PAM_SUCCESS);
 }
 
 // PAM entry point for setting user credentials (that is, to actually establish the authenticated
 // user's credentials to the service provider) - http://linux.die.net/man/3/pam_setcred
-PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return PAM_SUCCESS;
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(PAM_SUCCESS);
 }
 
 /* PAM entry point for authentication token changes - http://linux.die.net/man/3/pam_chauthtok */
-PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  return trusona_pam(pamh, flags, argc, argv);
+PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  return(trusona_pam(pamh, flags, argc, argv));
 }
