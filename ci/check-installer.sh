8d37b263917eb71d921257fa83259a85ec9fe759
Nikolas Thitu <nikolas@hauz.io>
2018-12-26 14:29:37 -0500

    updating check installer

--- ci/check-installer.sh
+++ ci/check-installer.sh
@@ -5,8 +5,7 @@ set -e
 wget -q https://github.com/libcheck/check/archive/0.12.0.tar.gz -O check-0.12.0.tar.gz
 tar xzf check-0.12.0.tar.gz
 cd check-0.12.0
-
 autoreconf --install
 ./configure
 make
-make check
+make install
