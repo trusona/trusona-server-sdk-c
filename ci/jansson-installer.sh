beac973ac32bd888a6c0c3be120a811de9962824
Nikolas Thitu <nikolas@hauz.io>
2018-12-26 14:27:07 -0500

    ci config updates

--- ci/jansson-installer.sh
+++ ci/jansson-installer.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
 
 ##
 ## grab latest Jansson
 ##
-wget http://www.digip.org/jansson/releases/jansson-2.10.tar.gz
+wget -q http://www.digip.org/jansson/releases/jansson-2.10.tar.gz -O jansson-2.10.tar.gz
 tar xzf jansson-2.10.tar.gz
-cd jansson-2.10 && ./configure && make && make check && make install
+cd jansson-2.10
+./configure
+make
+make check
+make install
