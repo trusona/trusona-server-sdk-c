3a2672b188e535d0e3e945ce9ffd742ed0c2bea5
Nikolas Thitu <nikolas@hauz.io>
2018-12-28 16:05:15 -0500

    updating tests

--- src/tests/check_file_perms_utils.c
+++ src/tests/check_file_perms_utils.c
@@ -16,7 +16,6 @@
 
 #include <check.h>
 #include "../utils.h"
-#include "../internal.h"
 
 const char *r_file = "test.empty.txt";
 const char *r_link = "test.link.txt";
@@ -32,30 +31,7 @@ void teardown(void)
   system("rm -f test.*.txt");
 }
 
-START_TEST(trim_removes_leading_and_trailing_whitespaces)
-{
-  ck_assert_str_eq(trim("     test"), "test");
-  ck_assert_str_eq(trim("   test  "), "test");
-  ck_assert_str_eq(trim("test     "), "test");
-  ck_assert_str_eq(trim("test"), "test");
-}
-END_TEST;
-
-START_TEST(trim_does_not_fail_on_blank_input)
-{
-  ck_assert_str_eq(trim("  "), "");
-  ck_assert_str_eq(trim(""), "");
-}
-END_TEST;
-
-START_TEST(trim_does_not_fail_on_NULL_input)
-{
-  ck_assert_pstr_eq(trim(NULL), NULL);
-  ck_assert_pstr_ne(trim(NULL), "");
-}
-END_TEST;
-
-START_TEST(file_perms_will_return_correct_value_for_regular_files)
+START_TEST(will_return_correct_value_for_regular_files)
 {
   system(concat_str("chmod 0600 ", r_file));
   ck_assert_int_eq(file_perms(r_file), 600);
@@ -74,7 +50,7 @@ START_TEST(file_perms_will_return_correct_value_for_regular_files)
 }
 END_TEST;
 
-START_TEST(file_perms_will_return_correct_value_for_sticky_files)
+START_TEST(will_return_correct_value_for_sticky_files)
 {
   system(concat_str("chmod 1600 ", r_file));
   ck_assert_int_eq(file_perms(r_file), 1600);
@@ -84,13 +60,13 @@ START_TEST(file_perms_will_return_correct_value_for_sticky_files)
 }
 END_TEST;
 
-START_TEST(file_perms_will_return_predefined_value_for_nonexistant_files)
+START_TEST(will_return_predefined_value_for_nonexistant_files)
 {
   ck_assert_int_eq(file_perms("no-such-file.txt"), -1);
 }
 END_TEST;
 
-START_TEST(file_perms_will_return_predefined_value_for_symbolic_links)
+START_TEST(will_return_predefined_value_for_symbolic_links)
 {
   system(concat_str("chmod 0600 ", r_link));
   ck_assert_int_eq(file_perms(r_link), -2);
@@ -103,26 +79,18 @@ END_TEST;
 Suite *utils_suite(void)
 {
   Suite *suite;
-  TCase *trim_tests;
-  TCase *perms_tests;
+  TCase *tests;
 
-  suite = suite_create("Utils");
+  suite = suite_create("File Permissions Tests");
+  tests = tcase_create("tests");
 
-  trim_tests  = tcase_create("trim-utils");
-  perms_tests = tcase_create("perm-utils");
+  tcase_add_checked_fixture(tests, setup, teardown);
+  tcase_add_test(tests, will_return_predefined_value_for_nonexistant_files);
+  tcase_add_test(tests, will_return_predefined_value_for_symbolic_links);
+  tcase_add_test(tests, will_return_correct_value_for_regular_files);
+  tcase_add_test(tests, will_return_correct_value_for_sticky_files);
 
-  tcase_add_test(trim_tests, trim_removes_leading_and_trailing_whitespaces);
-  tcase_add_test(trim_tests, trim_does_not_fail_on_blank_input);
-  tcase_add_test(trim_tests, trim_does_not_fail_on_NULL_input);
-
-  tcase_add_checked_fixture(perms_tests, setup, teardown);
-  tcase_add_test(perms_tests, file_perms_will_return_predefined_value_for_nonexistant_files);
-  tcase_add_test(perms_tests, file_perms_will_return_predefined_value_for_symbolic_links);
-  tcase_add_test(perms_tests, file_perms_will_return_correct_value_for_regular_files);
-  tcase_add_test(perms_tests, file_perms_will_return_correct_value_for_sticky_files);
-
-  suite_add_tcase(suite, trim_tests);
-  suite_add_tcase(suite, perms_tests);
+  suite_add_tcase(suite, tests);
 
   return(suite);
 }
---
+++ src/tests/check_owned_by_utils.c
@@ -0,0 +1,84 @@
+// Apache License. Version 2.0, January 2004
+//
+// Copyright (c) 2016, 2017, 2018 Trusona Inc (https://trusona.com) All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <check.h>
+#include "../utils.h"
+
+const char *r_file = "test.empty.txt";
+const char *r_link = "test.link.txt";
+
+void setup()
+{
+  system("echo 1 > test.empty.txt");
+  system("ln -sf test.empty.txt test.link.txt");
+}
+
+void teardown()
+{
+  system("rm -f test.*.txt");
+}
+
+START_TEST(will_return_predefined_ID_for_nonexistant_files)
+{
+  ck_assert_int_eq(owned_by("no-such-file.txt"), -4);
+}
+END_TEST;
+
+START_TEST(will_return_predefined_ID_for_symbolic_links)
+{
+  ck_assert_int_eq(owned_by(r_link), -2);
+}
+END_TEST;
+
+START_TEST(will_return_ID_matching_current_user_for_files_owned_by_current_user)
+{
+  ck_assert_int_eq(owned_by(r_file), geteuid());
+}
+END_TEST;
+
+Suite *utils_suite(void)
+{
+  Suite *suite;
+  TCase *tests;
+
+  suite = suite_create("File Ownership Tests");
+  tests = tcase_create("tests");
+
+  tcase_add_checked_fixture(tests, setup, teardown);
+  tcase_add_test(tests, will_return_predefined_ID_for_nonexistant_files);
+  tcase_add_test(tests, will_return_ID_matching_current_user_for_files_owned_by_current_user);
+  tcase_add_test(tests, will_return_predefined_ID_for_symbolic_links);
+
+  suite_add_tcase(suite, tests);
+
+  return(suite);
+}
+
+int main(void)
+{
+  int      failures;
+  Suite *  suite;
+  SRunner *runner;
+
+  suite    = utils_suite();
+  runner   = srunner_create(suite);
+  failures = -1;
+
+  srunner_run_all(runner, CK_NORMAL);
+  failures = srunner_ntests_failed(runner);
+  srunner_free(runner);
+  return((failures == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
