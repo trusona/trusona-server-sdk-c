8cde19e00cf9764ed9382ee86d5e9f4db0f1f965
Nikolas Thitu <nikolas@hauz.io>
2021-03-17 17:56:12 -0400

    various updates

--- debian/package-builder.sh
+++ debian/package-builder.sh
@@ -4,26 +4,26 @@ version=$1
 semver='^[0-9]+\.[0-9]+\.[0-9]+(-)?(\w)*$'
 
 if [[ $version =~ $semver ]]; then
-	:
+  :
 else
-	echo "Error: expecting tag version in semver format e.g. 1.2.3 or 4.5.6-beta"
-	exit -1
+  echo "Error: expecting tag version in semver format e.g. 1.2.3 or 4.5.6-beta"
+  exit -1
 fi
 
 checkinstall -y \
-	--install=no \
-	--maintainer="engineering@trusona.com" \
-	--pkgversion=1.0 \
-	--pkgrelease=1 \
-	--pkglicense=MIT \
-	--requires=libssl-dev,uuid-dev,libjansson-dev,libcurl4-openssl-dev,libjansson4,libuuid1,uuid-runtime,openssl \
-	--nodoc \
-	--delspec=no \
-	--type=debian \
-	--backup=no \
-	--autodoinst=no \
-	--strip=yes \
-	--stripso=yes \
-	--addso=yes \
-	--reset-uids=yes \
-	--deldoc=yes
+  --install=no \
+  --maintainer="engineering@trusona.com" \
+  --pkgversion=$version \
+  --pkgrelease=1 \
+  --pkglicense=MIT \
+  --requires=libssl-dev,uuid-dev,libjansson-dev,libcurl4-openssl-dev,libjansson4,libuuid1,uuid-runtime,openssl \
+  --nodoc \
+  --delspec=no \
+  --type=debian \
+  --backup=no \
+  --autodoinst=no \
+  --strip=yes \
+  --stripso=yes \
+  --addso=yes \
+  --reset-uids=yes \
+  --deldoc=yes
