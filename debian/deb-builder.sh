bb122d0707e1eeeb42c0616e3c92bdd064b28870
Nikolas Thitu <nikolas@hauz.io>
2021-03-18 12:47:27 -0400

    PR feedback

--- debian/deb-builder.sh
+++ debian/deb-builder.sh
@@ -4,6 +4,10 @@ set -e
 
 version=$1
 
+if [ -z $version ]; then
+  version="0.0.1-unspecified"
+fi
+
 if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+(-)?[a-z]*$ ]]; then
   echo "Error: expecting tag version in semver format e.g. 1.2.3 or 4.5.6-beta"
   exit -1
