9553f83e243de5902e2fadb87e4c0ef57824aa8f
Nikolas Thitu <nikolas@hauz.io>
2021-04-05 14:09:14 -0400

    dead code

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -3,20 +3,6 @@
 set -e
 
 ##
-## grab latest Jansson - https://github.com/akheron/jansson
-##
-version=2.13.1
-wget -q https://github.com/akheron/jansson/archive/v$version.tar.gz -O jansson-$version.tar.gz
-tar xzf jansson-$version.tar.gz
-cd jansson-$version
-autoreconf --install
-./configure
-make
-make check
-make install
-cd ..
-
-##
 ## Grab and install check - https://github.com/libcheck/check
 ##
 version=0.15.2
@@ -30,4 +16,4 @@ make install
 
 cd ..
 
-rm -rf check-* jansson-*
+rm -rf check-*
